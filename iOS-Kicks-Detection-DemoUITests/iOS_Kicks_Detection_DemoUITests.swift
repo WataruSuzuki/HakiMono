beb0e0ec93cf836044755a19352adae10c6604a6
WataruSuzuki <wataru0406@gmail.com>
2017-06-17 10:10:00 +0900

    Create Xcode project.

---
+++ iOS-Kicks-Detection-DemoUITests/iOS_Kicks_Detection_DemoUITests.swift
@@ -0,0 +1,36 @@
+//
+//  iOS_Kicks_Detection_DemoUITests.swift
+//  iOS-Kicks-Detection-DemoUITests
+//
+//  Created by 鈴木航 on 2017/06/19.
+//  Copyright © 2017年 WataruSuzuki. All rights reserved.
+//
+
+import XCTest
+
+class iOS_Kicks_Detection_DemoUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
