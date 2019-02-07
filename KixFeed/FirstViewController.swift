808546e0b039b1ff854e25de0f9159cf2b0be807
WataruSuzuki <wataru0406@gmail.com>
2019-02-07 22:20:00 +0900

    Created xcode project

---
+++ KixFeed/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  KixFeed
+//
+//  Created by 鈴木 航 on 2019/03/31.
+//  Copyright © 2019 WataruSuzuki. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ KixFeed/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  KixFeed
+//
+//  Created by 鈴木 航 on 2019/03/31.
+//  Copyright © 2019 WataruSuzuki. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
