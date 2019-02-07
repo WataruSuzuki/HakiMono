2156b6f86d7ef17424cdbd25c21dde8c58fa2351
WataruSuzuki <wataru0406@gmail.com>
2019-02-07 22:40:00 +0900

    Added embed framework target

---
+++ MonetizeKit/MonetizeKit.h
@@ -0,0 +1,19 @@
+//
+//  MonetizeKit.h
+//  MonetizeKit
+//
+//  Created by 鈴木 航 on 2019/03/31.
+//  Copyright © 2019 WataruSuzuki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MonetizeKit.
+FOUNDATION_EXPORT double MonetizeKitVersionNumber;
+
+//! Project version string for MonetizeKit.
+FOUNDATION_EXPORT const unsigned char MonetizeKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MonetizeKit/PublicHeader.h>
+
+
